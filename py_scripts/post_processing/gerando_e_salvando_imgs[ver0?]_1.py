f24df4dcce384800036835a2bf5667b4f591dc1f
Daniel-Orlandi <daniel0991@gmail.com>
2018-07-05 20:07:15 -0300

    Organizing .py files.

---
+++ py_scripts/post_processing/gerando_e_salvando_imgs[ver0?]_1.py
@@ -0,0 +1,54 @@
+import os
+import requests
+import numpy as np
+from scipy.misc import imread, imresize, imshow
+import hickle as hkl
+from kitti_settings import *
+from glob import glob
+import matplotlib.pyplot as plot
+import matplotlib.gridspec as gridspec
+from PIL import Image
+
+
+print 'inicio'
+pred = hkl.load('/home/daniel/AnacondaProjects/prednet-master_2/kitti_results/hdf-result/X_hat_model_nt15_f5_a3_pc.hkl')
+obs = hkl.load('/home/daniel/AnacondaProjects/prednet-master_2/kitti_results/hdf-result/X_test_model_nt15_f5_a3_pc.hkl')
+print pred.shape
+print obs.shape
+DATA_DIR = '/home/daniel/AnacondaProjects/prednet-master_2/kitti_results/prediction_plots_mono_tw/'
+if not os.path.exists(DATA_DIR):
+    os.mkdir(DATA_DIR)
+nt=15
+n_plot=15
+plot_idx = np.random.permutation(pred.shape[0])[:n_plot]
+print 'Iniciando loops'
+for i in range(1):
+    for t in range (nt): 
+        img = pred[i,t]    
+        plot.imshow(img,aspect='auto')
+        xlim, ylim = plot.xlim(), plot.ylim()
+        plot.xlim(xlim)
+        plot.ylim(ylim)   
+        plot.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
+        plot.savefig(DATA_DIR+'/'+'pred_'+str(i)+'_'+str(t)+'.png',dpi=600,bbox_inches='tight')
+
+for i in range(1):
+    for t in range (nt): 
+        img = obs[i,t]    
+        plot.imshow(img,aspect='auto')
+        xlim, ylim = plot.xlim(), plot.ylim()
+        plot.xlim(xlim)
+        plot.ylim(ylim)   
+        plot.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
+        plot.savefig(DATA_DIR+'/'+'obs_'+str(i)+'_'+str(t)+'.png',dpi=600,bbox_inches='tight')
+    
+print 'Imagens prontas'
+print 'calculando mse '
+mse_model = np.mean( (obs[:, 1:] - pred[:, 1:])**2 )  # look at all timesteps except the first
+mse_prev = np.mean( (obs[:, :-1] - obs[:, 1:])**2 )
+if not os.path.exists(RESULTS_SAVE_DIR): os.mkdir(RESULTS_SAVE_DIR)
+f = open(RESULTS_SAVE_DIR + 'prediction_scores.txt', 'w')
+f.write("Model MSE: %f\n" % mse_model)
+f.write("Previous Frame MSE: %f" % mse_prev)
+f.close()
+
---
+++ py_scripts/results_analysis/comparando_frames[ver2-fin]_3.py
@@ -0,0 +1,98 @@
+from skimage.measure import compare_ssim as ssim
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
+from glob import glob
+import os.path
+
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.mean((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
+ 
+def compare_images(imageA, imageB, title):
+	# compute the mean squared error and structural similarity
+	# index for the images
+	m = mse(imageA, imageB)
+	s = ssim(imageA, imageB,multichannel=True)
+ 
+	# setup the figure
+	fig = plt.figure(title)
+	plt.suptitle("MSE: %.6f, SSIM: %.4f" % (m, s))
+ 
+	# show first image
+	ax = fig.add_subplot(1, 2, 1)
+	plt.imshow(imageA, cmap = plt.cm.gray)
+	plt.axis("off")
+ 
+	# show the second image
+	ax = fig.add_subplot(1, 2, 2)
+	plt.imshow(imageB, cmap = plt.cm.gray)
+	plt.axis("off") 
+    # show the images
+	SAVE_DIR=str('/home/daniel/PredNet_artigo_jamc/kitti_results/prediction_plots_artigo_jamc_finetunned_scores/')
+	plt.savefig(SAVE_DIR+title,dpi=300,bbox_inches='tight')
+	plt.close()
+	print('saving!!')
+
+DATA_DIR='/home/daniel/PredNet_artigo_jamc/kitti_results/prediction_plots_artigo_jamc_finetunned/'
+DATA_DIR_2='/home/daniel/PredNet_artigo_jamc/kitti_results/prediction_plots_artigo_jamc_finetunned/'
+obs = glob(DATA_DIR_2+'obs_*_*') 
+pred = glob(DATA_DIR_2+'pred_*_*')
+obs = sorted(obs)
+pred= sorted(pred)
+obs_pred = zip(obs,pred)
+
+#Current frame SSIM e RMSE
+
+for each_obs, each_pred in obs_pred:
+    name_obs = os.path.basename(each_obs)
+    edit_name_obs = name_obs[0:5]
+    name_prev = os.path.basename(e)
+    name_pred = os.path.basename(each_pred)
+    name_file =  name_obs+'_'+name_pred
+    obs_processing = io.imread(each_obs)
+    pred_processing = io.imread(each_pred)
+    compare_images(obs_processing,pred_processing,str(name_file))
+    print 'image: '+str(name_file)+' saved!'
+
+
+#Previous Frame SSIM e RMSE	 
+	 
+i = 1
+for each_obs, each_pred in obs_pred:
+    name_obs = os.path.basename(each_obs)
+    current_frame_index = name_obs[6:7]
+    if current_frame_index == '0':
+        previous_frame_index = 0
+        
+
+
+
+    elif  17 < i <= 22 or 32 < i <= 37 or 47 < i <= 52:
+        current_frame_index = name_obs[6:8]
+        previous_frame_index = int(current_frame_index) - 1
+
+    else:
+        previous_frame_index = int(current_frame_index) - 1
+        
+    #name_prev = os.path.basename()
+    #name_pred = os.path.basename(each_pred)
+    i += 1
+    
+    new_file_name = name_obs[0:6]
+    name_file =  new_file_name + str(current_frame_index) + '_' + new_file_name + str(previous_frame_index) + '.png'   
+    obs_processing = io.imread(DATA_DIR_2 + new_file_name + str(current_frame_index)+'.png')
+    pred_processing = io.imread(DATA_DIR_2 + new_file_name + str(previous_frame_index)+'.png')
+    compare_images(obs_processing,pred_processing,str(name_file))
+    print 'image: '+str(name_file)+' saved!' 
+    
+print 'Pronto =)'
+
+
